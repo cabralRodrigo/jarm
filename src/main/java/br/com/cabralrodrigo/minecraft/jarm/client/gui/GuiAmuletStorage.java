011e5706996cbf5a887ac6469a2fc7474f22d32e
Rodrigo Cabral <rodrigopaivac@gmail.com>
2017-02-13 03:28:16 -0200

    Fixed storage amuler Part 2

--- src/main/java/br/com/cabralrodrigo/minecraft/jarm/client/gui/GuiAmuletStorage.java
+++ src/main/java/br/com/cabralrodrigo/minecraft/jarm/client/gui/GuiAmuletStorage.java
@@ -1,8 +1,9 @@
 package br.com.cabralrodrigo.minecraft.jarm.client.gui;
 
 import br.com.cabralrodrigo.minecraft.jarm.client.lib.LibGuiTextures;
-import br.com.cabralrodrigo.minecraft.jarm.common.inventory.container.ContainerJarmBase;
+import br.com.cabralrodrigo.minecraft.jarm.common.inventory.container.ContainerBase;
 import br.com.cabralrodrigo.minecraft.jarm.common.inventory.container.amulet.ContainerAmuletStorage;
+import br.com.cabralrodrigo.minecraft.jarm.common.inventory.impl.amulet.InventoryAmuletStorage;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumHand;
@@ -12,8 +13,8 @@ import org.lwjgl.opengl.GL11;
 
 @SideOnly(Side.CLIENT)
 public class GuiAmuletStorage extends GuiContainer {
-    public GuiAmuletStorage(EntityPlayer player, EnumHand hand) {
-        super(new ContainerAmuletStorage(player, hand));
+    public GuiAmuletStorage(EntityPlayer player, EnumHand hand, InventoryAmuletStorage inventoryAmuletStorage) {
+        super(new ContainerAmuletStorage(player, hand, inventoryAmuletStorage));
         this.xSize = 176;
         this.ySize = 222;
     }
@@ -29,8 +30,9 @@ public class GuiAmuletStorage extends GuiContainer {
 
     @Override
     protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
-        ContainerJarmBase container = (ContainerJarmBase) this.inventorySlots;
-        this.fontRendererObj.drawString(container.getJarmInventory().getDisplayName().getUnformattedText(), 8, 6, 4210752);
+        ContainerBase container = (ContainerBase) this.inventorySlots;
+
+        this.fontRendererObj.drawString(container.getInventoryName(), 8, 6, 4210752);
         this.fontRendererObj.drawString(container.getPlayer().inventory.getDisplayName().getUnformattedText(), 8, this.ySize - 96 + 2, 4210752);
     }
 }
--- src/main/java/br/com/cabralrodrigo/minecraft/jarm/common/item/amulet/ItemAmuletStorage.java
+++ src/main/java/br/com/cabralrodrigo/minecraft/jarm/common/item/amulet/ItemAmuletStorage.java
@@ -1,26 +1,34 @@
 package br.com.cabralrodrigo.minecraft.jarm.common.item.amulet;
 
-import br.com.cabralrodrigo.minecraft.jarm.common.Jarm;
+import br.com.cabralrodrigo.minecraft.jarm.common.capability.CapabilityProviderItemHandler;
 import br.com.cabralrodrigo.minecraft.jarm.common.item.ItemAmuletBase;
 import br.com.cabralrodrigo.minecraft.jarm.common.lib.LibGui;
 import br.com.cabralrodrigo.minecraft.jarm.common.lib.LibItems;
-import br.com.cabralrodrigo.minecraft.jarm.common.util.EnumHandHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+
+import javax.annotation.Nullable;
 
 public class ItemAmuletStorage extends ItemAmuletBase {
     public ItemAmuletStorage() {
         super(LibItems.AMULET_STORAGE);
     }
 
+    @Nullable
+    @Override
+    public ICapabilityProvider initCapabilities(ItemStack stack, @Nullable NBTTagCompound nbt) {
+        return new CapabilityProviderItemHandler(6 * 9);
+    }
+
     @Override
     public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
-        if (!world.isRemote)
-            player.openGui(Jarm.instance, LibGui.AMULET_STORAGE, world, EnumHandHelper.ToInt(hand), 0, 0);
+        this.openGui(player, world, LibGui.AMULET_STORAGE, hand);
 
         return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
     }
